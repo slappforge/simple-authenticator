7ff792cc4e820ba42b7b7e1d7b1b7a94f80b006e
rnavagamuwa <randikanavagamuwa@gmail.com>
2018-06-05 16:54:59 +0530

    Fixed the imported slapforge-sdk dependency

--- simple-authenticator-sigma-sample/signup-lambda.js
+++ simple-authenticator-sigma-sample/signup-lambda.js
@@ -1,6 +1,6 @@
 let AWS = require('aws-sdk');
 let connectionManager = require('./ConnectionManager');
-let SL = require('@slappforge/slappforge-sdk');
+let SL_AWS = require('slappforge-sdk-aws');
 const rds = new SL_AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
 
