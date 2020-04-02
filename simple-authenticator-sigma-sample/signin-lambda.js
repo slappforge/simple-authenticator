d0c178c26a23b899cb78ccebf201a4652b51b05e
Udith <uditharosha@gmail.com>
2020-04-02 11:14:01 +0530

    Modified to use async-await syntax

--- simple-authenticator-sigma-sample/signin-lambda.js
+++ simple-authenticator-sigma-sample/signin-lambda.js
@@ -3,25 +3,33 @@ let connectionManager = require('./ConnectionManager');
 let SL_AWS = require('slappforge-sdk-aws');
 const rds = new SL_AWS.RDS(connectionManager);
 
-exports.handler = function (event, context, callback) {
- let successfullyLoggedIn = false;
+exports.handler = async (event) => {
 	let inserts = [event.email, event.password];
 
 	// You can pass the existing connection to this function.
 	// A new connection will be created if it's not present as the third param 
 	// You must always end the DB connection after it's used
-	rds.query({
-		instanceIdentifier: 'authDatabase',
-		query: 'SELECT * FROM users WHERE Email = ? AND Password = ?',
-		inserts: inserts
-	}, function (error, results, connection) {
-		if (error) {
-			throw error;
-		} else {
-			successfullyLoggedIn = results.length > 0;
-		}
+	try {
+		let results = await new Promise((resolve, reject) => {
+			rds.query({
+				instanceIdentifier: 'authDatabase',
+				query: 'SELECT * FROM users WHERE Email = ? AND Password = ?',
+				inserts: inserts
+			}, (error, results, connection) => {
+				connection.end();
+				if (error) {
+					reject(error);
+				} else {
+					resolve(results.length);
+				}
+			});
+		});
+		let successfullyLoggedIn = (results.length > 0);
+		console.log("Successfully logged:", successfullyLoggedIn);
+		return successfullyLoggedIn;
 
-		connection.end();
-		callback(null, successfullyLoggedIn);
-	});
-}
+	} catch (err) {
+		console.log("Error occurred while logging in with email:", email, err);
+		return false;
+	}
+};
